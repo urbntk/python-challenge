542abf1ea21e1b25301f2666aa56d594c1372ab8
Urbantek <urbantek@users-MacBook.mshome.net>
2019-06-04 13:05:16 -0700

    two hundredth commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,46 @@
-#PyBank main.py
+import csv
+df_budget = "budget_data.csv"
+total_months = 0
+total_revenue = 0
+prev_revenue = 0
+month_of_change = []
+revenue_change_list = []
+greatest_decrease = ["", 9999999999999999999]
+biggest_increase = ["", 0]
+
+
+
+with open(df_budget) as revenue_data:
+    reader = csv.DictReader(revenue_data)
+
+    for row in reader:
+        total_months = total_months + 1
+        total_revenue = total_revenue + int(row["Profit/Losses"])
+        revenue_change = int(row["Profit/Losses"]) - prev_revenue
+        prev_revenue = int(row["Profit/Losses"])
+        revenue_change_list = revenue_change_list + [revenue_change]
+        month_of_change = month_of_change + [row["Date"]]
+
+        if (revenue_change > biggest_increase[1]):
+            biggest_increase[0] = row["Date"]
+            biggest_increase[1] = revenue_change
+        if (revenue_change < greatest_decrease[1]):
+            greatest_decrease[0] = row["Date"]
+            greatest_decrease[1] = revenue_change
+
+revenue_avg = sum(revenue_change_list) / len(revenue_change_list)
+
+output = (
+    f"\nFinancial Analysis\n"
+    f"------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total Revenue: ${total_revenue}\n"
+    f"Average Change: ${revenue_avg}\n"
+    f"Greatest Increase in Revenue: {biggest_increase[0]} (${biggest_increase[1]})\n"
+    f"Greatest Decrease in Revenue: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
+
+print(output)
+
+df_analysis = "budget_analysis.txt"
+with open(df_analysis, "w") as txt_file:
+    txt_file.write(output)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,49 @@
-# PyPollmain.py
+# Incorporated the csv module
+import csv
+
+df_election = "election_data.csv"
+
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+
+with open(df_election) as election_data:
+    reader = csv.DictReader(election_data)
+
+    for row in reader:
+        total_votes = total_votes + 1
+        candidate_name = row["Candidate"]
+
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
+
+file_to_output = "election_results.txt"
+with open(file_to_output, "w") as txt_file:
+    election_results = (
+        f"\n\nElection Results\n"
+        f"--------------------\n"
+        f"Total Votes: {total_votes}\n"
+        f"--------------------\n")
+    print(election_results, end="")
+    txt_file.write(election_results)
+
+    for candidate in candidate_votes:
+        votes = candidate_votes.get(candidate)
+        vote_percentage = float(votes) / float(total_votes) * 100
+        if (votes > winning_count):
+            winning_count = votes
+            winning_candidate = candidate
+        voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
+        print(voter_output, end="")
+        txt_file.write(voter_output)
+
+    winning_candidate_summary = (
+        f"--------------------\n"
+        f"President of the World: {winning_candidate}\n"
+        f"--------------------\n")
+    print(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
